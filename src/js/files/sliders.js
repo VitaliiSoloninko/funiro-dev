711727b5bd0029a860ad76b71c110a25533316fa
vitaliypulupovuch <vitaliypulupovuch@gmail.com>
2023-09-28 17:02:39 +0200

    5 Gallery HTML + CSS + JS Part 3

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -131,6 +131,11 @@ function initSliders() {
 				disableOnInteraction: false,
 			},
 			*/
+			// Кнопки "вліво/вправо"
+			navigation: {
+				prevEl: '.slider-arrows .slider-arrow_prev',
+				nextEl: '.slider-arrows .slider-arrow_next',
+			},
 			// Пагінація
 			pagination: {
 				el: '.slider-rooms__dotts',
@@ -143,85 +148,7 @@ function initSliders() {
 				draggable: true,
 			},
 			*/
-			// Кнопки "вліво/вправо"
-			navigation: {
-				prevEl: '.slider-rooms .slider-arrow_prev',
-				nextEl: '.slider-rooms .slider-arrow_next',
-			},
-			/*
-			// Брейкпоінти
-			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// Події
-			on: {
-			}
-		});
-	}
 
-	if (document.querySelector('.rooms2__slider')) { // Вказуємо склас потрібного слайдера
-		// Створюємо слайдер
-		new Swiper('.rooms2__slider', { // Вказуємо склас потрібного слайдера
-			// Підключаємо модулі слайдера
-			// для конкретного випадку
-			modules: [Navigation, Pagination],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 24,
-			watchOverflow: true,
-			//autoHeight: true,
-			speed: 800,
-			//touchRatio: 0,
-			//simulateTouch: false,
-			loop: true,
-			watchOverflow: true,
-			loopAdditionalSliders: 5,
-			preloadImages: false,
-			parallax: true,
-			// lazy: true,
-			/*
-			// Ефекти
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
-			// Пагінація
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			// Скроллбар
-			/*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
-			// Кнопки "вліво/вправо"
-			navigation: {
-				prevEl: '.rooms2__slider .swiper-button-prev',
-				nextEl: '.rooms2__slider .swiper-arrow_next',
-			},
 			/*
 			// Брейкпоінти
 			breakpoints: {
@@ -250,34 +177,8 @@ function initSliders() {
 		});
 	}
 }
-// Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
-function initSlidersScroll() {
-	let sliderScrollItems = document.querySelectorAll('.swiper_scroll');
-	if (sliderScrollItems.length > 0) {
-		for (let index = 0; index < sliderScrollItems.length; index++) {
-			const sliderScrollItem = sliderScrollItems[index];
-			const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
-			const sliderScroll = new Swiper(sliderScrollItem, {
-				observer: true,
-				observeParents: true,
-				direction: 'vertical',
-				slidesPerView: 'auto',
-				freeMode: {
-					enabled: true,
-				},
-				scrollbar: {
-					el: sliderScrollBar,
-					draggable: true,
-					snapOnRelease: false
-				},
-				mousewheel: {
-					releaseOnEdges: true,
-				},
-			});
-			sliderScroll.scrollbar.updateSize();
-		}
-	}
-}
+
+
 
 window.addEventListener("load", function (e) {
 	// Запуск ініціалізації слайдерів
