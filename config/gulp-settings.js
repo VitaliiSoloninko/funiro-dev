bc2cd5ba5d0788f34517d19a7dfdac67703bf180
22051988Di <vitalii.soloninko@gmail.com>
2024-05-12 18:13:36 +0200

    v2.0.2	fix files

--- config/gulp-settings.js
+++ config/gulp-settings.js
@@ -1,10 +1,10 @@
 // Отримуємо ім'я папки проекту
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
+import * as nodePath from 'path'
+const rootFolder = nodePath.basename(nodePath.resolve())
 
 // Шляхи до папки з вихідними даними та папки з результатом
-const buildFolder = `./dist`;
-const srcFolder = `./src`;
+const buildFolder = `./dist`
+const srcFolder = `./src`
 
 // Шляхи до папок та файлів проекту
 export const path = {
@@ -14,7 +14,7 @@ export const path = {
 		css: `${buildFolder}/css/`,
 		images: `${buildFolder}/img/`,
 		fonts: `${buildFolder}/fonts/`,
-		files: `${buildFolder}/files/`
+		files: `${buildFolder}/files/`,
 	},
 	src: {
 		html: `${srcFolder}/*.html`,
@@ -32,15 +32,15 @@ export const path = {
 	rootFolder: rootFolder,
 	srcFolder: srcFolder,
 	// Шлях до потрібної папки на віддаленому сервері.
-	ftp: ``
+	ftp: ``,
 	// Приклад: завантажити в папку 2022 далі в папку з назвою проєкту
 	// ftp: `2022/${rootFolder}`
-};
+}
 
 // Налаштування FTP з'єднання
 export const configFTP = {
-	host: "", // Адреса FTP сервера
-	user: "", // Ім'я користувача
-	password: "", // Пароль
-	parallel: 5 // Кількість одночасних потоків
+	host: '', // Адреса FTP сервера
+	user: '', // Ім'я користувача
+	password: '', // Пароль
+	parallel: 5, // Кількість одночасних потоків
 }
